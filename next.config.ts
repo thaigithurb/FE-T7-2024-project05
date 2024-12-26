07ee76b0a44ab378a438a346ff72a63419c7e454
thaigithurb <23631761.thai@student.iuh.edu.vn>
2024-12-26 17:47:43 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,4 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-module.exports = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-}
-
 export default nextConfig;
